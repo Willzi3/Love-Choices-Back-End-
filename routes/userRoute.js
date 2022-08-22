b5b80e95127ef87f1113cb5c05722a74ca1a6a73
Willzi3 <williamszachary787@gmail.com>
2022-08-22 22:31:16 +0200

    hosted on heroku

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -18,7 +18,7 @@ router.get("/", (req, res) => {
     }
   });
   
-  // Gets one user
+  // Gets one user by id
   router.get("/:id", (req, res) => {
     try {
       con.query(`SELECT * FROM users WHERE user_id ='${req.params.id}'`, (err, result) => {
